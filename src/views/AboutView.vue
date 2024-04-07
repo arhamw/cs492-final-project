2b20e66b2ace01e54614bd422c9c30fc713da13f
Arhum Waqar <arhumwaqar@Abidas-MacBook-Air.local>
2024-04-07 18:23:33 -0400

    Added references in p tag

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import insta_logo from '../assets/instagram_logo.png'
-import references from '../assets/references.txt'
 </script>
 
 <template>
@@ -43,9 +42,61 @@ import references from '../assets/references.txt'
       </a>
     </div>
     <div>
-      <h2 class="glow">
-        <a :href="references" download="references.txt">References (click me)</a>
-      </h2>
+      <h2 class="glow">References</h2>
+      <p>
+        (1) Breland, A., Corn, D., Friedman, D., Younes, L., de Leon, R., Sokolove, M., Métraux, J.,
+        McShane, J., Vesoulis, A., Coghill, A., & Lanard, N. (2019, March 15). The bizarre and
+        terrifying case of the "deepfake" video that helped bring an African nation to the brink.
+        Mother Jones. Retrieved April 7, 2024, from
+        https://www.motherjones.com/politics/2019/03/deepfake-gabon-ali-bongo/<br /><br />
+        (2) Buljan, M. (2021, November 15). Gamification For Learning: Strategies And Examples.
+        eLearning Industry. Retrieved March 12, 2024, from
+        https://elearningindustry.com/gamification-for-learning-strategies-and-examples<br /><br />
+        (3) Canadian Security Intelligence Service. (2023, October). The Evolution of
+        Disinformation: A Deepfake Future. His Majesty the King in Right of Canada, as represented
+        by the Minister of Public Safety. Retrieved from
+        https://www.canada.ca/en/security-intelligence-service/corporate/publications/the-evolution-of-disinformation-a-deepfake-future<br /><br />
+        (4) Guess, A. M., et al. (2020). A digital media literacy intervention increases discernment
+        between mainstream and false news in the United States and India. Proceedings of the
+        National Academy of Sciences, 117(27), 15536–15545.
+        https://doi.org/10.1073/pnas.1920498117<br /><br />
+        (5) Heikkilä, M., & Douglas, W. (2022, December 19). How to spot AI-generated text. MIT
+        Technology Review. Retrieved April 7, 2024, from
+        https://www.technologyreview.com/2022/12/19/1065596/how-to-spot-ai-generated-text/<br /><br />
+        (6) Healey, J. (2024, February 26). Beverly Hills Middle School Rocked by AI-Generated Nude
+        Images of Students. Los Angeles Times. Retrieved from
+        https://www.latimes.com/california/story/2024-02-26/beverly-hills-middle-school-is-the-atest-to-be-rocked-by-deepfake-scandal<br /><br />
+        (7) Islam, M. S., et al. (2020). COVID-19–Related Infodemic and Its Impact on Public Health:
+        A Global Social Media Analysis. The American Journal of Tropical Medicine and Hygiene,
+        103(4), 1621-1629. https://doi.org/10.4269/ajtmh.20-0812<br /><br />
+        (8) Kennedy, B., Tyson, A., & Saks, E. (2023, February 15). Public awareness of artificial
+        intelligence in Everyday Activities. Pew Research Center Science & Society.
+        https://www.pewresearch.org/science/2023/02/15/public-awareness-of-artificial-intelligence-in-everyday-activities/
+        <br /><br />
+        (9) Kong, H. (2024, February 5). Deepfake Scammer Walks Off with $25 Million in
+        First-of-its-Kind AI Heist. Ars Technica. Retrieved from
+        https://www.arstechnica.com/information-technology/2024/02/deepfake-scammer-walks-off-with-25-million-in-first-of-its-kind-ai-heist<br /><br />
+        (10) Lord, K. M., & Vogt, K. (2021). Strengthen Media Literacy to Win the Fight Against
+        Misinformation. Stanford Social Innovation Review. https://doi.org/10.48558/PY1B-3G51<br /><br />
+        (11) Responsibility & Safety. Google DeepMind. (n.d.).
+        https://deepmind.google/about/responsibility-safety/<br /><br />
+        (12) Ryan-Mosley, T. (2023, October 4). How Generative AI Is Boosting the Spread of
+        Disinformation and Propaganda. MIT Technology Review. Retrieved from
+        https://www.technologyreview.com/2023/10/04/1080801/generative-ai-boosting-disinformation-and-propaganda-freedom-house/<br /><br />
+        (13) Sailer, M., Homner, L. The Gamification of Learning: a Meta-analysis. Educ Psychol Rev
+        32, 77–112 (2020). https://doi.org/10.1007/s10648-019-09498-w<br /><br />
+        (14) Savin, J. (2022, October 6). Deepfake Porn Is on the Rise – and Everyday Women Are the
+        Target. Cosmopolitan. Retrieved from
+        https://www.cosmopolitan.com/uk/reports/a41534567/what-are-deepfakes/<br /><br />
+        (15) Serrano, J. (2022, May 27). Please Don’t Invest in This Crypto Scam Because Deepfake
+        Elon Musk Told You To. Gizmodo. Retrieved from
+        https://www.gizmodo.com/elon-musk-deepfake-invest-bitcoin-scam-bitvex-1848982652<br /><br />
+        (16) We Stand for Integrity. The Trust Project. (2023, July 31).
+        https://thetrustproject.org/<br /><br />
+        (17) Wesolowski, K. (2023, April 9). Fact check: How can I spot AI-generated images? – DW –
+        04/09/2023. DW. Retrieved April 7, 2024, from
+        https://www.dw.com/en/fact-check-how-can-i-spot-ai-generated-images/a-65252602<br />
+      </p>
     </div>
   </div>
 </template>
